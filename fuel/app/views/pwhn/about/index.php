4ddc4144637f7f9fbdd4e7b216cf71d87758adb8
cassidyro <112151915+cassidyro@users.noreply.github.com>
2023-03-29 17:10:58 -0600

    About page
    profile photo and bio

--- fuel/app/views/pwhn/about/index.php
+++ fuel/app/views/pwhn/about/index.php
@@ -7,6 +7,60 @@
         <hr>
         <h1>ABOUT</h1>
         <hr>
+        <h3>Pets With Human Names is founded by four Colorado State University students studying computer science with a passion for animals.</h3>
+
+
+        
+        <div class="container">
+            <div class = "image">
+                <br>
+
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
+            </div>
+            <div class="text">
+            <h5>Cassidy Roberts is a sophomore at Colorado State University pursuing a degree in Computer Science. With a passion for web development, specifically front-end development, Cassidy is constantly seeking new challenges and opportunities to expand her knowledge and skills in this field.
+            Apart from her academic pursuits, Cassidy also has a furry companion named Yogi. Yogi is a one-year-old mixed breed dog, who Cassidy adores and considers an important part of her life.
+            </h5> 
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class = "image">
+                <br>
+
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
+            </div>
+            <div class="text">
+            <h5>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae, bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
+            </h5> 
+            </div>
+        </div>
+
+        <div class="container">
+            <div class = "image">
+                <br>
+
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
+            </div>
+            <div class="text">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae, bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
+            </h5> 
+            </div>
+        </div>
+
+        <div class="container">
+            <div class = "image">
+                <br>
+
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
+            </div>
+            <div class="text">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae, bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
+            </h5> 
+            </div>
+        </div>
     </header>
 
 </html>
