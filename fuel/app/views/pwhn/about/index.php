c7b41f3ed02b4bd2cfb0d8f766c7134b902256c9
Alex Glimco <alexpcg@rams.colostate.edu>
2023-03-30 17:13:12 -0600

    about page setup

--- fuel/app/views/pwhn/about/index.php
+++ fuel/app/views/pwhn/about/index.php
@@ -5,63 +5,94 @@
 
     <header>
         <hr>
-        <h1>ABOUT</h1>
+        <h1>About Us</h1>
         <hr>
-        <h3>Pets With Human Names is founded by four Colorado State University students studying computer science with a passion for animals.</h3>
-
-
-        
-        <div class="container">
-            <div class = "image">
-                <br>
-
-            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
-            </div>
-            <div class="text">
-            <h5>Cassidy Roberts is a sophomore at Colorado State University pursuing a degree in Computer Science. With a passion for web development, specifically front-end development, Cassidy is constantly seeking new challenges and opportunities to expand her knowledge and skills in this field.
-            Apart from her academic pursuits, Cassidy also has a furry companion named Yogi. Yogi is a one-year-old mixed breed dog, who Cassidy adores and considers an important part of her life.
-            </h5> 
-            </div>
-        </div>
-
-
-        <div class="container">
-            <div class = "image">
-                <br>
-
-            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
-            </div>
-            <div class="text">
-            <h5>
-            Alex Glimco
-            I wish to use my degree, education, and skills from CSU to better myself as I grow in the industry and ultimately aim to work for a forward-thinking company that values the environment, innovation, and human equality.
-            </h5> 
-            </div>
-        </div>
-
-        <div class="container">
-            <div class = "image">
-                <br>
-
-            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
-            </div>
-            <div class="text">
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae, bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
-            </h5> 
-            </div>
-        </div>
-
-        <div class="container">
-            <div class = "image">
-                <br>
-
-            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2" border="0" width="150px" height="200px"></a>
-            </div>
-            <div class="text">
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae, bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
-            </h5> 
-            </div>
-        </div>
+        <h2>Who is behind Pets with Human Names?</h2>
+        <h3>Pets With Human Names is founded by four Colorado State University students studying computer science with a
+            passion for animals.</h3>
+        <hr>
     </header>
+
+    <div class="container">
+        <div class="image">
+            <br>
+
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2"
+                    border="0" height="400px"></a>
+        </div>
+        <div class="text">
+            <h4>Cassidy Roberts</h4>
+            <h5>Cassidy Roberts is a sophomore at Colorado State University pursuing a degree in Computer Science. With
+                a passion for web development, specifically front-end development, Cassidy is constantly seeking new
+                challenges and opportunities to expand her knowledge and skills in this field.
+                Apart from her academic pursuits, Cassidy also has a furry companion named Yogi. Yogi is a one-year-old
+                mixed breed dog, who Cassidy adores and considers an important part of her life.
+            </h5>
+        </div>
+    </div>
+    <hr>
+
+    <div class="container">
+        <div class="image">
+            <br>
+
+            <a href="https://ibb.co/94czQyN"><img src="https://avatars.githubusercontent.com/u/97863393?v=4"
+                    alt="IMG-5361-2" border="0" height="400px"></a>
+        </div>
+        <div class="text">
+            <h4>Alex Glimco</h4>
+            <h5>
+                I wish to use my degree, education, and skills from CSU to better myself as I grow in the industry and
+                ultimately aim to work for a forward-thinking company that values the environment, innovation, and human
+                equality.
+            </h5>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="image">
+            <br>
+            <h4>Jack</h4>
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2"
+                    border="0" height="400px"></a>
+        </div>
+        <div class="text">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae,
+                bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor
+                blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis
+                fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit
+                amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id
+                pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate
+                leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
+            </h5>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="image">
+            <br>
+            <h4>Brianna</h4>
+            <a href="https://ibb.co/94czQyN"><img src="https://i.ibb.co/KsbRTVX/IMG-5361-2.jpg" alt="IMG-5361-2"
+                    border="0" height="400px"></a>
+        </div>
+        <div class="text">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, iaculis vitae ante vitae,
+                bibendum dictum orci. Proin dapibus urna sed aliquet facilisis. Pellentesque posuere urna ut dolor
+                blandit, eget interdum metus tincidunt. Mauris quis pharetra lorem. Cras lacus mauris, euismod quis
+                fermentum a, fringilla sed mauris. Curabitur metus ante, ullamcorper vel fermentum maximus, porta sit
+                amet risus. Vestibulum a tellus in nunc porta condimentum. Pellentesque accumsan sagittis odio, id
+                pretium ligula consequat bibendum. Aliquam non elit sed tellus vestibulum dictum. Morbi vel vulputate
+                leo, sed ornare nisi. Phasellus ut leo eget orci rhoncus sodales eu id massa.
+            </h5>
+        </div>
+    </div>
+
+    <hr>
+
+    <footer>
+        <hr>
+        <p>&copy; Pets with Human Names, 2023</p>
+        <hr>
+    </footer>
 
 </html>
--- fuel/app/views/pwhn/colortable/index.php
+++ fuel/app/views/pwhn/colortable/index.php
@@ -1,1 +1,12 @@
-colortable
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+    <header>
+        <hr>
+        <h1>Color Table</h1>
+        <hr>
+    </header>
+
+</html>
