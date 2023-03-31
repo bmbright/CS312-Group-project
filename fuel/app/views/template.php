d28d7dcc5e77159f0c710a32eeff9f92fb7ecfa9
Alex Glimco <alexpcg@rams.colostate.edu>
2023-03-30 21:23:18 -0600

    template file fix

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -5,24 +5,30 @@
     </title>
 
     <meta charset="UTF-8">
-    <meta name="author" content="Alex Glimco, Cassidy Roberts, Brianna Bright, Jack Matthews">
-    <meta name="description" content="Pets with Human Names FuelPHP Template">
-    <meta name="keywords" content="Pets with Human Names, Fuel, Template, HTML, CS312, Colorado State University">
+    <meta name="author" content="Alex Glimco - alexpcg - 831965240">
+    <meta name="description" content="FuelPHP Template">
+    <meta name="keywords" content="Alex Glimco, Fuel, Template, HTML, CS312, Colorado State University">
     <?php echo Asset::css($css) ?>
 </head>
 
 <header>
-<div class="logo"><a><img src="https://i.imgur.com/1GjCd3S.png" alt="PetsWithHumanNamesLogo" height="200px"></a></div>
+    <div class="logo">
+        <a><img src="https://i.imgur.com/1GjCd3S.png" alt="PetsWithHumanNamesLogo" height="200px"></a>
+    </div>
     <h1>Pets with Human Names</h1>
+
     <ul>
-        <?php echo '<li><a href="https://cs.colostate.edu:4444/~bmbright/m1/pwhn/index/'.$yeet.'">Home</a></li>' ?>
-        <?php echo '<li><a href="https://cs.colostate.edu:4444/~bmbright/m1/pwhn/index/about/'.$yeet.'">About</a></li>' ?>
-        <?php echo '<li><a href="https://cs.colostate.edu:4444/~bmbright/m1/pwhn/index/colorTable/'.$yeet.'">ColorTable</a>'?>
+        <li><a href="https://cs.colostate.edu:4444/~alexpcg/m1/index/pwhn/">Home</a></li>
+        <li><a href="https://cs.colostate.edu:4444/~alexpcg/m1/index/pwhn/about/about?direction=about">About</a></li>
+        <li><a href="https://cs.colostate.edu:4444/~alexpcg/m1/index/pwhn/colortable/colortable?direction=colortable">ColorTable</a> </li>
     </ul>
 </header>
 
 <body>
+
     <main>
         <?php echo $content ?>
     </main>
+
+
 </body>
