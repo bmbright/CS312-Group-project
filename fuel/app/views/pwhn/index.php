e8cd0fe6cd2e9af9de5942ad74b2d03a1878fe99
cassidyro <112151915+cassidyro@users.noreply.github.com>
2023-03-30 18:42:40 -0600

    Homepage
    home page

--- fuel/app/views/pwhn/index.php
+++ fuel/app/views/pwhn/index.php
@@ -3,10 +3,16 @@
 
 <body>
 
-    <header>
+<header>
         <hr>
         <h1>HOME</h1>
         <hr>
+        <h3>Welcome to Pets Without Names!</h3>
+        <p>We are a pet shelter located in Fort Collins, Colorado, run by four passionate computer science students at Colorado State University. Our mission is to provide a safe and loving home for pets in need, while also giving them the chance to find their forever families.</p>
+        <p>At Pets Without Names, we believe that every pet deserves a chance at a happy and fulfilling life, regardless of their age, breed, or background. That's why we work tirelessly to ensure that each and every one of our animals receives the care and attention they deserve.</p>
+        <p>Whether you're looking to adopt a new furry friend or simply want to support our cause, we invite you to explore our website and learn more about Pets Without Names. From our adoptable pets and volunteer opportunities to our fundraising events and educational programs, there are plenty of ways to get involved and make a difference in the lives of animals.</p>
+        <h2>Some of our furry friends</h2>
+        
     </header>
 
     <footer>
