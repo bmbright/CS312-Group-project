2366673e9ea8ca80ba9e402931cbfb26a8d8904d
Alex Glimco <alexpcg@rams.colostate.edu>
2023-03-30 20:29:43 -0600

    Update ColorTable Page

--- fuel/app/views/pwhn/colortable/index.php
+++ fuel/app/views/pwhn/colortable/index.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<body>
-
+<div class="page-header">
     <header>
         <hr>
-        <h1>Color Table</h1>
+        <h2>Color Table</h2>
         <hr>
     </header>
+</div>
 
-    <form action="https://cs.colostate.edu:4444/~bmbright/m1/pwhn/index/colorTable/" mothod="POST">
+<body>
+    <form action="" method="GET">
         <p>
             <label for="rows">Enter number of Rows/Columns:</label>
             <input type="number" name="rows" id="rows" min=1 max=26 required>
         </p>
         <p>
-            <label for="colors">Number of Colors:</label>
+            <label for="colors">Enter number of Colors:</label>
             <input type="number" name="colors" id="colors" min=1 max=10 required>
         </p>
         <input type="submit">
     </form>
 </body>
+
+<footer>
+    <hr>
+    <p>&copy; Pets with Human Names, 2023</p>
+    <p>Colorado State University - CS312</p>
+    <hr>
+</footer>
+
 </html>
